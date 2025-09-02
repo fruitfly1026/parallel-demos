d4f2000739c728c0316738c41f303ebe74b20b0c
ahmedtaimoor <86569495+ahmedtaimoor@users.noreply.github.com>
2025-09-02 16:18:52 -0400

    walltime using gettimeofday.

--- ProgAssignments/Monte-Carlo-OMP/main.c
+++ ProgAssignments/Monte-Carlo-OMP/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <sys/time.h>
 
 #include "util.h"
 
 static const double I_true = 0.7468241328124271;
 
-double monte_carlo_pi(long long N, unsigned int seed);
+// double monte_carlo_pi(long long N, unsigned int seed);
 double monte_carlo_pi_omp(long long N, unsigned int seed);
-double monte_carlo_integral(long long N, unsigned int seed);
+// double monte_carlo_integral(long long N, unsigned int seed);
 double monte_carlo_integral_omp(long long N, unsigned int seed);
 
 int main(int argc, char* argv[]) {
@@ -27,13 +27,15 @@ int main(int argc, char* argv[]) {
     }
 
     // Run Task 1: Monte Carlo Pi Estimation
-    clock_t start = clock();
-    // TODO: replace the function name with the seq/omp version
+    struct timeval start;
+    struct timeval end;
+
+    gettimeofday(&start, NULL);
     double pi_hat = monte_carlo_pi_omp(N, seed);
-    clock_t end = clock();
+    gettimeofday(&end, NULL);
 
     double abs_err = fabs(pi_hat - M_PI);
-    double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    double elapsed = (double)(end.tv_sec - start.tv_sec) +  (double)(end.tv_usec - start.tv_usec)/1000000.0f;
 
     printf("Samples N = %lld, Seed = %u\n", N, seed);
     printf("===== Task 1: Monte Carlo Pi Estimation =====\n");
@@ -42,12 +44,11 @@ int main(int argc, char* argv[]) {
     printf("Time elapsed = %.6f sec\n", elapsed);
 
     // Run Task 2: Monte Carlo Integration
-    start = clock();
-    // TODO: replace the function name with the seq/omp version
+    gettimeofday(&start, NULL);
     double I_hat = monte_carlo_integral_omp(N, seed);
-    end = clock();
+    gettimeofday(&end, NULL);
     abs_err = fabs(I_hat - I_true);
-    elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    elapsed = (double)(end.tv_sec - start.tv_sec) +  (double)(end.tv_usec - start.tv_usec)/1000000.0f;
     printf("===== Task 2: Monte Carlo Integration =====\n");
     printf("Estimated I = %.12f, True I = %.12f\n", I_hat, I_true);
     printf("Absolute error = %.6e\n", abs_err);
